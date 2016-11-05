9c70d1c16225af5c0f8508f8c9004c0b831bfc7d
dsumer <domi.sumer@gmail.com>
2016-11-05 19:45:37 +0100

    updated theme, added img shortcode, added content to the about page, adjusted urls

--- javascripts/main.js
+++ javascripts/main.js
@@ -12,3 +12,18 @@ new Clipboard('.article pre', {
         return trigger.firstChild.innerText;
     }
 });
+
+var article = document.getElementsByClassName('article')[0];
+var nodes = document.getElementsByTagName("a"), i = nodes.length;
+var regExp = new RegExp("//" + location.host + "($|/)");
+while(i--){
+    var link = nodes[i];
+    var href = link.href;
+    var isLocal = (href.substring(0,4) === "http") ? regExp.test(href) : true;
+    if (!isLocal) {
+        if (!link.getAttribute('target')) {
+            link.setAttribute('target', '_blank');
+        }
+        link.classList.add('external');
+    }
+}
